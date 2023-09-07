48b110a28bd153824deebd60811fe65cef9beeba
carbal23 <mcarbal23@gmail.com>
2023-09-07 10:01:13 -0500

    disable prettier eslint

--- backend/src/modules/breeds/dto/breed.dto.ts
+++ backend/src/modules/breeds/dto/breed.dto.ts
@@ -2,9 +2,8 @@ import { IsNotEmpty } from 'class-validator';
 import { SubBreedDto } from 'src/modules/sub-breed/dto/sub-breed.dto';
 
 export class BreedDto {
-    @IsNotEmpty()
-    readonly name: string;
+  @IsNotEmpty()
+  readonly name: string;
 
-    readonly subBreeds: SubBreedDto[];
-
+  readonly subBreeds: SubBreedDto[];
 }
